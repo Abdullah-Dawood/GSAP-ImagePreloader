6d16397f80693d95f8c088825056745fdd75e003
abdullah <abdullahrock480@gmail.com>
2026-05-22 13:16:19 +0500

    feat: implemented loading counter animation

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ import Image2 from "./assets/optimized/image2.jpg";
 import Image3 from "./assets/optimized/image3.jpg";
 import Image4 from "./assets/optimized/image4.jpg";
 import Image5 from "./assets/optimized/image5.jpg";
+import { SplitText } from "gsap/SplitText";
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, SplitText);
 
 const imagesToLoad = [Image1, Image2, Image3, Image4, Image5];
 
@@ -34,6 +35,8 @@ function preloadImages(srcs: string[]) {
 function App() {
   const containerRef = useRef<HTMLDivElement>(null);
   const [assetsReady, setAssetsReady] = useState(false);
+
+  const [percentageCountDown, setPercentageCountDown] = useState("000");
 
   useEffect(() => {
     preloadImages(imagesToLoad).then(() => {
@@ -90,17 +93,40 @@ function App() {
     { dependencies: [assetsReady], scope: containerRef }
   );
 
+  useGSAP(
+    () => {
+      if (!assetsReady) return;
+
+      const counter = { value: 0 };
+
+      const updateText = () => {
+        const roundedValue = Math.round(counter.value);
+        const formattedValue = roundedValue.toString().padStart(3, "0");
+
+        setPercentageCountDown(formattedValue);
+      };
+
+      const percentageTimeline = gsap.timeline();
+
+      percentageTimeline.to(counter, {
+        keyframes: [
+          { value: 20, duration: 0.6, ease: "power2.in" },
+          { value: 90, duration: 0.7, ease: "power1.inOut" },
+          { value: 100, duration: 0.8, ease: "power3.out" },
+        ],
+        onUpdate: updateText,
+      });
+
+      return () => percentageTimeline.kill();
+    },
+    { dependencies: [assetsReady], scope: containerRef }
+  );
+
   return (
     <div
       ref={containerRef}
       className="bg-[#ede4dd] h-screen w-screen flex items-center justify-center"
     >
-      {!assetsReady && (
-        <div className="fixed inset-0 flex items-center justify-center">
-          Loading...
-        </div>
-      )}
-
       {assetsReady && (
         <div className="fixed inset-0 flex items-center justify-center img-container">
           {imagesToLoad.map((src, index) => (
@@ -115,6 +141,10 @@ function App() {
           ))}
         </div>
       )}
+
+      <p className="fixed top-[32%] right-[38%] text-3xl  text-[#ff0001] header-txt">
+        {percentageCountDown}
+      </p>
     </div>
   );
 }
